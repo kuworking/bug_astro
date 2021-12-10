4ddeee90beba2ab13e4248540c08b113cdfc8d59
kuworking <kuworking@gmail.com>
2021-12-10 23:46:15 +0100

    .

---
+++ src/components/js/Cookies.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react'
+import './Cookies.css'
+
+const wait = ms => new Promise((res, rej) => setTimeout(() => res('timed'), ms))
+
+const grantit = () => {
+  if (typeof window !== 'undefined') {
+    if (window.dataLayer)
+      window.dataLayer.push('consent', 'update', {
+        ad_storage: 'granted',
+        analytics_storage: 'granted',
+      })
+    // if (window.fbq) window.fbq('consent', 'grant') facebook
+  }
+}
+
+export const Cookies = () => {
+  // show the panel, only if js is enabled and cookie not there
+  useEffect(() => {
+    const item = window.localStorage.getItem('kuworking_cookies')
+    if (!item) {
+      ;(async () => {
+        await wait(1000)
+        document.getElementById('cookie_panel').style.display = 'flex'
+        await wait(50)
+        document.getElementById('cookie_panel').style.opacity = 1
+      })()
+    } else if (item === 'granted') grantit()
+  }, [])
+
+  // hide the panel and send the consent to google
+  const agree = (agree = true) => {
+    document.getElementById('cookie_panel').style.display = 'none'
+
+    if (agree) {
+      window.localStorage.setItem('kuworking_cookies', 'granted')
+      grantit()
+    }
+  }
+
+  const disagree = () => agree(false)
+
+  return (
+    <div id="cookie_panel" data-nosnippet="data-nosnippet">
+      <h2>Datos almacenados</h2>
+      <div>
+        Utilizamos Google Analytics con datos anonimizados para generar estadísticas de la página web (con tu permiso)
+      </div>
+      <div>
+        <button id="cookie_ok" className="OK" onClick={agree}>
+          De Acuerdo
+        </button>
+        <button id="cookie_no" onClick={disagree}>
+          No quiero
+        </button>
+      </div>
+    </div>
+  )
+}
